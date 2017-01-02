4625a150a36303d0522d800317a4bdc326e7eeff
Edgar Cuesta <edgarcufe@gmail.com>
2017-01-02 15:26:30 -0500

    Ejercicio 4 de LazyFoo, Eventos de teclado

--- FirstEvent/closeEvent.cpp
+++ FirstEvent/closeEvent.cpp
@@ -22,101 +22,101 @@ SDL_Surface* gHelloWorld = NULL;
 
 int main(int argc, char* argv[])
 {
-  //Start up SDL and create window
-  if(!init())
+    //Start up SDL and create window
+    if(!init())
     {
-      printf("Failed to initialize!\n");
+        printf("Failed to initialize!\n");
     }
-  else
+    else
     {
-      //Load media
-      if(!loadMedia())
-	{
-	  printf("Failed to load media!\n");
-	}
-      else
-	{
-	  //Main loop flag
-	  bool quit = false;
-	  //Event handler
-	  SDL_Event e;
-	  //While application is running
-	  while(!quit)
-	    {
-	      //Handle events on queue
-	      while(SDL_PollEvent(&e) != 0)
-		{
-		  //User requests quit
-		  if(e.type == SDL_QUIT)
-		    {
-		      quit = true;
-		    }
-		}
-	      //Apply the image
-	      SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-	      //Update the Surface
-	      SDL_UpdateWindowSurface(gWindow);
-	    }
-	}
+        //Load media
+        if(!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+            //Main loop flag
+            bool quit = false;
+            //Event handler
+            SDL_Event e;
+            //While application is running
+            while(!quit)
+            {
+                //Handle events on queue
+                while(SDL_PollEvent(&e) != 0)
+                {
+                    //User requests quit
+                    if(e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+                //Apply the image
+                SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+                //Update the Surface
+                SDL_UpdateWindowSurface(gWindow);
+            }
+        }
     }
-  //Free resources and close SDL
-  close();
-  return 0;
+    //Free resources and close SDL
+    close();
+    return 0;
 }
 
 bool init()
 {
-  //Initialization flag
-  bool success = true;
-  //Initialize SDL
-  if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    //Initialization flag
+    bool success = true;
+    //Initialize SDL
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
     {
-      printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-      success = false;
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
     }
-  else
+    else
     {
-      //Create window
-      gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-      if(gWindow == NULL)
-	{
-	  printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-	  success = false;
-	}
-      else
-	{
-	  //Get Window Surface
-	  gScreenSurface = SDL_GetWindowSurface(gWindow);
-	}
+        //Create window
+        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(gWindow == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            //Get Window Surface
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
     }
-  return success;
+    return success;
 }
 
 bool loadMedia()
 {
-  //Loading success flag
-  bool success = true;
-  //Load splash image
-  gHelloWorld = SDL_LoadBMP("images/hello_world.bmp");
-  if(gHelloWorld == NULL)
+    //Loading success flag
+    bool success = true;
+    //Load splash image
+    gHelloWorld = SDL_LoadBMP("images/hello_world.bmp");
+    if(gHelloWorld == NULL)
     {
-      printf("Unable to load image %s! SDL_Error:%s\n", "images/hello_world.bmp", SDL_GetError());
-      success = false;
+        printf("Unable to load image %s! SDL_Error:%s\n", "images/hello_world.bmp", SDL_GetError());
+        success = false;
     }
-  return success;
+    return success;
 
 }
 
 void close()
 {
-  //Deallocate surface
-  SDL_FreeSurface(gHelloWorld);
-  gHelloWorld = NULL;
+    //Deallocate surface
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
 
-  //Destroy window
-  SDL_DestroyWindow(gWindow);
-  gWindow = NULL;
+    //Destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
 
-  //Quit SDL Subsystems
-  SDL_Quit();
+    //Quit SDL Subsystems
+    SDL_Quit();
 }
